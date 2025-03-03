f2be230eabc5cc4c4b139fcf85b2bebde4d017f8
hejy47 <hejy47@github.com>
2025-03-03 11:19:25 +0800

    add ast parser generated by antlr4.

---
+++ pyverilog/vparser_anltr4/parser.py
@@ -0,0 +1,21 @@
+import sys
+from antlr4 import *
+from SystemVerilogLexer import SystemVerilogLexer
+from SystemVerilogParser import SystemVerilogParser
+from svast2pyverilog import SVastToPyverilogVisitor
+
+def parse_verilog(file_path):
+    input_stream = FileStream(file_path, encoding='utf-8')
+    
+    lexer = SystemVerilogLexer(input_stream)
+    stream = CommonTokenStream(lexer)
+    
+    parser = SystemVerilogParser(stream)
+    context = parser.source_text()
+    
+    visitor = SVastToPyverilogVisitor()
+    ast = visitor.visit(context)
+
+if __name__ == "__main__":
+    arg = sys.argv[1]
+    parse_verilog(arg)
---
+++ pyverilog/vparser_anltr4/svast2pyverilog.py
@@ -0,0 +1,52 @@
+from antlr4 import *
+from SystemVerilogLexer import SystemVerilogLexer
+from SystemVerilogParser import SystemVerilogParser
+from SystemVerilogParserVisitor import SystemVerilogParserVisitor
+from pyverilog.vparser.ast import *
+
+class SVastToPyverilogVisitor(SystemVerilogParserVisitor):
+    def visitChildren(self, node):
+        results = []
+        if node == None: return results
+        for i in range(node.getChildCount()):
+            child = node.getChild(i)
+            result = child.accept(self)
+            results.append(result)
+        return results
+
+    def visitSource_text(self, ctx):
+        children = self.visitChildren(ctx)
+        lineno = ctx.start.line
+        ast = Source("", children, lineno)
+        return ast
+    
+    def visitDescription(self, ctx):
+        children = self.visitChildren(ctx)
+        lineno = ctx.start.line
+        ast = Description(children, lineno)
+        return ast
+    
+    def visitModule_declaration(self, ctx):
+        lineno = ctx.start.line
+        name, paramlist, portlist = self.visitModule_header(ctx.module_header())
+        items = self.visitChildren(ctx.module_item())
+        ast = ModuleDef(name, paramlist, portlist, items, lineno)
+        return ast
+    
+    def visitModule_header(self, ctx):
+        identifier = ctx.module_identifier().getText()
+        paramlist = Paramlist(self.visitChildren(ctx.parameter_port_list()))
+        portlist = Portlist(self.visitChildren(ctx.list_of_port_declarations()))
+
+        return identifier, paramlist, portlist
+    
+    def visitParameter_port_declaration(self, ctx):
+        pass
+    
+    def visitPort_decl(self, ctx):
+        lineno = ctx.start.line
+        port = Port(ctx.getText(), width=None, dimensions=None, type=None, lineno=lineno)
+        return port
+    
+    def visitModule_item(self, ctx):
+        return None
